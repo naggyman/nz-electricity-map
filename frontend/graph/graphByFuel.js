07e1c266dff0d48756d2d43725a342f406829c14
morgan.french-stagg <morgan@french.net.nz>
2024-05-11 09:21:53 +1200

    simplify chart aggregation logic

--- frontend/graph/graphByFuel.js
+++ frontend/graph/graphByFuel.js
@@ -16,63 +16,57 @@ const keyOrder = [
 
 export async function getChartSeriesDataByFuel(liveGenData, data, siteFilter = [], islandFilter = [], zoneFilter = [], fuelFilter = []){
     var tradingPeriodTimestamps = Object.keys(data);
+    var outputGenerationByFuel = {};
+    
+    var filteredGeneratorList = liveGenData.generators.filter(generator => {
+        return isGeneratorInFilter(generator, siteFilter, islandFilter, zoneFilter);
+    });
+    
+    // find all of the fuels that we will need to chart
     var allFuels = [];
-    var fuels = {};
-
-    var summarisedGenerationPerTradingPeriod = tradingPeriodTimestamps.map(tradingPeriod => summariseGenerationByFuelForThisTradingPeriod(liveGenData, data[tradingPeriod], allFuels, siteFilter, islandFilter, zoneFilter))
-
-    summarisedGenerationPerTradingPeriod.forEach(tradingPeriodGeneration => {
-        allFuels.forEach(fuel => {
-            if (fuels[fuel] === undefined) {
-                fuels[fuel] = [];
+    filteredGeneratorList.forEach(generator => {
+        generator.units.forEach(unit => {
+            const fuelKey = Object.keys(FUELS_KEY).at(Object.values(FUELS_KEY).indexOf(unit.fuel));
+            if (!allFuels.includes(fuelKey)) {
+                allFuels.push(fuelKey);
+                outputGenerationByFuel[fuelKey] = [];
             }
-
-            //if data was missing for this trading period, we want to leave a gap in the graph
-            if(Object.keys(tradingPeriodGeneration).length === 0){
-                fuels[fuel].push(null);
-                return;
-            }
-
-            fuels[fuel].push(tradingPeriodGeneration[fuel] || 0);
         });
     });
 
-    return orderedFuelList(fuels).map(fuel => getDatapointForFuelFilteredGraph(fuel, fuels, fuelFilter));
-}
+    tradingPeriodTimestamps.forEach(tradingPeriodTimestamp => {
+        var gens = Object.keys(data[tradingPeriodTimestamp]);
+        if(gens.length === 0){ //do generation data for this trading period - leave a gap in the graph
+            allFuels.forEach(fuel => {
+                outputGenerationByFuel[fuel].push(null);
+            });
+            return;
+        }
 
-function summariseGenerationByFuelForThisTradingPeriod(liveGenData, tradingPeriodData, allFuels, siteFilter, islandFilter, zoneFilter){
-    if(Object.keys(tradingPeriodData).length === 0){
-        return {};
-    }
+        var generationData = data[tradingPeriodTimestamp];
+        var thisTradingPeriodSummaryByFuel = {};
 
-    var generationByFuel = {};
-
-    var generatorList = filterGeneratorList(liveGenData, tradingPeriodData, siteFilter, islandFilter, zoneFilter);
-
-    /**
-     * Covers the case where there is data for some generators in this trading period,
-     * just none for the filters specified.
-     * 
-     * In this situation we still want to show 0 on the graph, as there is data that proves there was no generation.
-     * As opposed to missing data, where we can't say whether or not generation occurred in that period.
-     */
-    if(generatorList.length === 0){
-        allFuels.forEach(fuel => {generationByFuel[fuel] = 0});
-    } else {
-        generatorList.forEach(generator => {
-            if (generationByFuel[generator.fuel] === undefined) {
-                generationByFuel[generator.fuel] = 0;
+        filteredGeneratorList.forEach(generator => {
+            // find the data from this trading period for this generator
+            var genData = generationData.filter(gen => gen.site === generator.site);
+            if(genData.length == 0){
+                return;
             }
-    
-            generationByFuel[generator.fuel] += generator.gen;
-    
-            if (!allFuels.includes(generator.fuel)) {
-                allFuels.push(generator.fuel);
-            }
+
+            genData.forEach(generationDataPoint => {
+                // append it - to the relevant fuel
+                thisTradingPeriodSummaryByFuel[generationDataPoint.fuel] = (thisTradingPeriodSummaryByFuel[generationDataPoint.fuel] || 0) + generationDataPoint.gen;
+            });
         });
-    };
+ 
+        // for all fuels we will graph, find their summarised generation for this trading period
+        allFuels.forEach(fuel => {
+            outputGenerationByFuel[fuel].push(thisTradingPeriodSummaryByFuel[fuel] || 0);
+        });
+    });
 
-    return generationByFuel;
+    return orderedFuelList(outputGenerationByFuel)
+        .map(fuel => getHighchartDatapointForFuel(fuel, outputGenerationByFuel, fuelFilter));
 }
 
 export function getTooltipForFuelFilteredGraph(){
@@ -90,9 +84,19 @@ export function getTooltipForFuelFilteredGraph(){
     })
 
     this.points.forEach(point => {
-        const percentage = (totalGeneration != 0) ? Math.round(point.y / totalGeneration * 100) : 0;
+        if(point.y == 0){
+            body += `
+                <span style="color: ${point.color}">\u25CF</span> 
+                ${point.series.name}: 0 MW
+                <br>`;
+        } else {
+            const percentage = (totalGeneration != 0) ? Math.round(point.y / totalGeneration * 100) : 0;
 
-        body += `<span style="color: ${point.color}">\u25CF</span> ${point.series.name}: <b>${displayMegawattsOrGigawatts(point.y)}</b> (${percentage}%)<br>`;
+            body += `
+                <span style="color: ${point.color}">\u25CF</span> 
+                ${point.series.name}: <b>${displayMegawattsOrGigawatts(point.y)}</b> (${percentage}%)
+                <br>`;
+        }
     })
 
     const renewablePercentage = (totalGeneration != 0) ? Math.round(renewableGeneration / totalGeneration * 100) : 0;
@@ -108,7 +112,7 @@ function orderedFuelList(fuels){
     return keyOrder.filter(fuel => Object.keys(fuels).includes(fuel));
 }
 
-function getDatapointForFuelFilteredGraph(fuel, fuels, fuelFilter){
+function getHighchartDatapointForFuel(fuel, fuels, fuelFilter){
     var fuelName = FUELS_KEY[fuel] || fuel;
     return {
         name: fuelName,
@@ -118,9 +122,7 @@ function getDatapointForFuelFilteredGraph(fuel, fuels, fuelFilter){
     }
 }
 
-function isGeneratorInFilter(site, liveGenData, siteFilter, islandFilter, zoneFilter){
-    var generator = (liveGenData.generators.filter((gen) => gen.site === site))[0];
-
+function isGeneratorInFilter(generator, siteFilter, islandFilter, zoneFilter){
     if(siteFilter.length > 0 && !siteFilter.includes(generator.site)){
         return false;
     }
@@ -134,10 +136,4 @@ function isGeneratorInFilter(site, liveGenData, siteFilter, islandFilter, zoneFi
     }
 
     return true;
-}
-
-function filterGeneratorList(liveGenData, generators, siteFilter, islandFilter, zoneFilter){
-    return generators.filter(generator => {
-        return isGeneratorInFilter(generator.site, liveGenData, siteFilter, islandFilter, zoneFilter);
-    });
 }
