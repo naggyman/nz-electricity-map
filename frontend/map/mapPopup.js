419c1d349a0eeb7b2f7837492d0e2b0b534e154d
morgan.french-stagg <morgan@french.net.nz>
2024-06-23 20:47:14 +1200

    "View Generation Chart" now defaults to 'Today' instead of 'last 3 hours'

--- frontend/map/mapPopup.js
+++ frontend/map/mapPopup.js
@@ -36,7 +36,7 @@ export function populateGeneratorPopup(generatorData, lastUpdated) {
         ${generatorData.operator} <span class="badge text-bg-primary">${generatorData.site}</span><br><br>
         ${populateGenerationData(generatorData)}
         <i> Last Updated: ${lastUpdated}</i>
-        <br><a href="index.html?site=${generatorData.site}&timeframe=-3h&redirect=true">View Generation Chart</a>
+        <br><a href="index.html?site=${generatorData.site}&timeframe=-0d&redirect=true">View Generation Chart</a>
             `;
 
     if(generatorData.units.some((unit) => unit.outage.length > 0)){
