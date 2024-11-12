af03509a6bba0bf6650d965ea79517ca9ffd0504
morgan.french-stagg <morgan@french.net.nz>
2024-11-12 19:45:48 +1300

    make previous non-comissioned outage re-appear

--- backend/services/Outages.py
+++ backend/services/Outages.py
@@ -18,6 +18,15 @@ class Outages:
             self.outages = []
 
         # add a fake outage for generators not yet commissioned
+        self.outages.append({
+            "orgId": "6d8eda92-ed4c-4a1d-b816-eb0f9fafba4e",
+            "outageBlock": "TAC_0",
+            "timeStart": "2024-01-01T00:00:00+13:00",
+            "timeEnd": "2024-11-12T14:30:00+13:00",
+            "mwattRemaining": 0,
+            "mwattLost": 51.4
+            })
+
             
         # Ruakaka BESS
         self.outages.append({
