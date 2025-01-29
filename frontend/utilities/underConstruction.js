6190f8773d50c3302ee811f254f6011ae95e2126
morgan.french-stagg <morgan.french-stagg@xero.com>
2025-01-30 08:04:47 +1300

    adjust

--- frontend/utilities/underConstruction.js
+++ frontend/utilities/underConstruction.js
@@ -43,7 +43,7 @@ export const underConstruction = [
         "capacityMW": 38,
         "capacityMWp": 42,
         "yearlyGenerationGWh": 69,
-        "openBy": "2024-12-31",
+        "openBy": "2025-01-30",
         "location": {
             "lat": -38.02688240581342,
             "long": 177.18354792348188
@@ -61,7 +61,7 @@ export const underConstruction = [
         "capacityMW": 14,
         "capacityMWp": 16.7,
         "yearlyGenerationGWh": 28,
-        "openBy": "2024-12-31",
+        "openBy": "2025-01-30",
         "location": {
             "lat": -36.11523760044438,
             "long": 174.02666511899804
@@ -79,7 +79,7 @@ export const underConstruction = [
         "capacityMWp": 62.6,
         "capacityMW": 47.2,
         "yearlyGenerationGWh": 97,
-        "openBy": "2024-12-30",
+        "openBy": "2025-01-30",
         "location": {
             "lat": -43.730843339796294,
             "long": 171.78955859810299
