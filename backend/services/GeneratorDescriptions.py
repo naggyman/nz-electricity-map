75ee5a57e9eab59ea28262a58af74f9625468703
morgan.french-stagg <morgan.french-stagg@xero.com>
2025-09-24 21:57:13 +1200

    small mistake prevening outage for Twin Rivers appearing properly

--- backend/services/GeneratorDescriptions.py
+++ backend/services/GeneratorDescriptions.py
@@ -75,7 +75,7 @@ class GeneratorDescriptions:
             case '664e64d1-416c-446b-814d-e73cab27b769':
                 return "Oji Fibre Solutions"
             case '20cc9dfe-6dca-41b9-91a7-be627f71880d':
-                return "Ranui Generation"
+                return "Rānui Generation"
             case default:
                 return None
             
